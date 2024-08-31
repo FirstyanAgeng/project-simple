0cf26a347fab5bdb8aae711f327b7871d832c321
firstyan ageng <firztyan@gmail.com>
2024-09-01 00:21:58 +0700

    first init

---
+++ Backend/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const cors = require("cors");
+
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const database = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "simple_db",
+  port: 3307,
+});
+
+database.connect((err) => {
+  if (err) throw error;
+  console.log("database connected");
+});
+
+// get data
+app.get("/api/products", (req, res) => {
+  database.query("SELECT * FROM simple_item", (err, rows) => {
+    if (err) throw err;
+    res.json({
+      success: true,
+      message: "getting produk item",
+      data: rows,
+    });
+  });
+});
+
+// add data
+app.post("/api/products", (req, res) => {
+  const { nama_produk, stock } = req.body;
+
+  const query = "INSERT INTO simple_item (nama_produk, stock) VALUES (?, ?)";
+  database.query(query, [nama_produk, stock], (err, result) => {
+    if (err) throw err;
+    res.json({
+      success: true,
+      message: "Product added successfully",
+      data: result,
+    });
+  });
+});
+
+app.put("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { nama_produk, stock } = req.body;
+
+  const query =
+    "UPDATE simple_item SET nama_produk = ?, stock = ? WHERE id = ?";
+  database.query(query, [nama_produk, stock, id], (err, result) => {
+    if (err) throw err;
+    res.json({
+      success: true,
+      message: "Product updated successfully",
+      data: result,
+    });
+  });
+});
+
+app.delete("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+
+  const query = "DELETE FROM simple_item WHERE id = ?";
+  database.query(query, [id], (err, result) => {
+    if (err) throw err;
+    res.json({
+      success: true,
+      message: "Product deleted successfully!",
+      data: result,
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log("server running on port : ", port);
+});
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [namaProduk, setNamaProduk] = useState("");
+  const [stock, setStock] = useState("");
+
+  const endpoint = "http://localhost:3001/api/products";
+
+  const fetchData = async () => {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    setProducts(data.data);
+    console.log(data);
+  };
+
+  const addProduct = async () => {
+    await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ nama_produk: namaProduk, stock }),
+    });
+    fetchData();
+  };
+
+  const updateProduct = async (id) => {
+    const newProduct = prompt("Enter new produk name");
+    const newStock = prompt("Enter new stock");
+
+    await fetch(`${endpoint}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ nama_produk: newProduct, stock: newStock }),
+    });
+    fetchData(); // Refresh the product list after updating
+  };
+
+  const deleteProduct = async (id) => {
+    await fetch(`${endpoint}/${id}`, {
+      method: "DELETE",
+    });
+    fetchData(); // Refresh the product list after deleting
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Nama Produk"
+        value={namaProduk}
+        onChange={(e) => setNamaProduk(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Stock"
+        value={stock}
+        onChange={(e) => setStock(e.target.value)}
+      />
+      <button onClick={addProduct}>Add Product</button>
+
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Nama Produk</th>
+            <th>Stock</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.nama_produk}</td>
+              <td>{product.stock}</td>
+              <td>
+                <button onClick={() => updateProduct(product.id)}>
+                  Update
+                </button>
+                <button onClick={() => deleteProduct(product.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
